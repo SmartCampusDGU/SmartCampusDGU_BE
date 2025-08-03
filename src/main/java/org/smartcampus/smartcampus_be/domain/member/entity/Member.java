b7de11c91423b2af7345d0f19f36c3ba13e7cb75
eeddiinn <gdw4013@naver.com>
2025-08-04 00:05:34 +0900

    [FEAT] Member 엔티티 및 Role Enum 구현

---
+++ src/main/java/org/smartcampus/smartcampus_be/domain/member/entity/Member.java
@@ -0,0 +1,32 @@
+package org.smartcampus.smartcampus_be.domain.member.entity;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+@Entity
+@Table(name = "members")
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@AllArgsConstructor
+@Builder
+public class Member {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "member_id", nullable = false, unique = true)
+    private String memberId;  // 아이디
+
+    @Column(nullable = false)
+    private String password;  // 암호화된 비밀번호
+
+    @Column(nullable = false)
+    private String name; // 계정명
+
+    private String description; // 설명
+
+    @Enumerated(EnumType.STRING)
+    @Column(nullable = false)
+    private Role role;
+}
---
+++ src/main/java/org/smartcampus/smartcampus_be/domain/member/entity/Role.java
@@ -0,0 +1,5 @@
+package org.smartcampus.smartcampus_be.domain.member.entity;
+
+public enum Role {
+    ADMIN, USER;
+}
