0258f983698c4a7bb5d344c3bf7417268a931640
minij02 <mjryu00211@gmail.com>
2025-09-03 20:46:44 +0900

    [fix] @Value import 추가

--- src/main/java/org/smartcampus/smartcampus_be/global/common/jwt/SecurityConfig.java
+++ src/main/java/org/smartcampus/smartcampus_be/global/common/jwt/SecurityConfig.java
@@ -3,6 +3,7 @@ package org.smartcampus.smartcampus_be.global.common.jwt;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
